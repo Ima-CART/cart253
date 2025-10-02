143250378227b0d0a6851bc9560bbcb64d954c5b
Ima-CART <Sandra_Williams93@hotmail.com>
2025-10-02 04:52:30 -0400

    Drawing Eye brows

--- Jams/Art-Jam/js/script.js
+++ Jams/Art-Jam/js/script.js
@@ -13,7 +13,9 @@
 */
 function setup() {
     createCanvas(640, 640);
-
+    //setting the units of the arcs
+   //setting up the angle of the arcs
+   angleMode(degrees);
 
 }
 
@@ -28,17 +30,41 @@ function draw() {
     //starting of with naming all the required functions for this project
     Drawbackground();
     Drawface();
-    Draweyes();
     Drawbrow();
+    Draweyes();
     Drawneck();
     Drawhair();
     Drawsweater();
     Drawears();
     Drawnose();
     Drawmouth();
+    
+    // Starting of wiht a nice light blue background
+function Drawbackground(){
+     push();
+     background(143, 234, 255);
+     pop();
 
- // Starting of wiht a nice light blue background
+    }
+    //Draw face
+    function Drawface(){
+     push();
+     noStroke();
+     fill("#8d6241ff");
+     ellipse(320,280,200,270);
+     pop();
+    
+    }
+    function Drawbrow() {
+     push();
+     noFill();
+     stroke("#2b2418ff");
+     strokeWeight(3);
+     arc(275, 270, 50, 30, 154, 270.1);  
+     arc(365, 270, 50, 30, 154, 270.1); 
+     pop(); 
+    }
 
- // A circle in the centre of the canvas
- ellipse(320, 320, 450, 580);
+
+    
 }
