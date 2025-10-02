0bb038a8dd7dfb4c3a3a930b45cf8fa9806744a4
Ima-CART <Sandra_Williams93@hotmail.com>
2025-10-02 08:33:23 -0400

    Organizing my scripts and function

--- Jams/Art-Jam/js/script.js
+++ Jams/Art-Jam/js/script.js
@@ -13,15 +13,14 @@
 */
 function setup() {
     createCanvas(640, 640);
-    //setting the units of the arcs
-    //setting up the angle of the arcs
+    //setting up the units and angle of the arcs
     angleMode(degrees);
     
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Setting all the draw functions in the draw
 */
 
 
@@ -44,55 +43,47 @@ function draw() {
     
     // Starting of wiht a nice light blue background
     function Drawbackground(){
-        push();
-        background(143, 234, 255);
-        pop();
-        
-    }
+    push();
+    background(143, 234, 255);
+    pop();    
+}
     
     //Drawing the hair
-    function Drawhair(){
-        push();
-        noStroke();
-        fill("#957011ff");
-        ellipse(320,285,270,350); // this is the hair in the back.
-        pop();
-    }
+function Drawhair(){
+    push();
+    noStroke();
+    fill("#957011ff");
+    ellipse(320,285,270,350); // this is the hair in the back.
+    pop();
+}
+
     //The hoodie hood will be here since it's in the back
-    function Drawhood (){
-        push();
-        stroke("#0619c5ff")
-        strokeWeight(12)
-        fill("#000c7aff");
-        ellipse(320, 470, 230, 150);
-
-        pop();
-
-
-
-    }
+function Drawhood (){
+    push();
+    stroke("#0619c5ff")
+    strokeWeight(12)
+    fill("#000c7aff");
+    ellipse(320, 470, 230, 150);
+    pop();
+}
     
     //Draw face
-    function Drawface(){
-        push();
-        noStroke();
-        fill("#8d6241ff");
-        ellipse(320,280,200,270);
-        pop();
-        
-    }
-    
-    
+function Drawface(){
+    push();
+    noStroke();
+    fill("#8d6241ff");
+    ellipse(320,280,200,270);
+    pop();
+}
     
     //Drawing the inner tshirt
-    function Drawtshirt() {
+function Drawtshirt() {
     push();
     noStroke();
     fill("#ffff");
     rect(180, 450, 280, 250,80)
-    fill("#000")
-    //fill("#f1f5ffff")
-    ellipse(320, 460, 85, 65)
+    fill("#f1f5ffff")
+    ellipse(320, 460, 85, 65)//the neckline
     pop();
 }
 
@@ -106,7 +97,7 @@ function Drawneck(){
        
 }
     //Drawing Eyebrows    
-    function Drawbrow() {
+function Drawbrow() {
         push();
         noFill();
         stroke("#2b2418ff");
@@ -116,44 +107,42 @@ function Drawneck(){
         pop(); 
     }
     
-    function Draweyes() {
-        push();
+    //Drawing the eyes
+function Draweyes() {
+    push();    
+    noStroke();       
+    //Starting of with the left eye
+    ellipse(275, 275, 55, 30);//the white part of the eye 
+    push(); //I don't want the colors to fall on the second eye. I verified without push and pop it does 
+    fill("#5c3427f2");
+    ellipse(275,275,30,30) //the brown eyeball
+    fill("#000000ff")
+    ellipse(275,275,15,15)  // the iris
+    fill("#fff")
+    ellipse(280,268,8,8) // the reflection
+    pop();
         
-        noStroke();
+    //The right eye
+    ellipse(365, 275, 55, 30);
+    push();// Keeping the same consistency as the first eye
+    fill("#5c3427f2");
+    ellipse(365,275,30,30) //the brown eyeball
+    fill("#000000ff")
+    ellipse(365,275,15,15)  //the iris
+    fill("#fff")
+    ellipse(370,268,8,8) // the reflection
         
-        //Starting of with the left eye
-        ellipse(275, 275, 55, 30);//the white part of the eye 
-        push(); //I don't want the colors to fall on the second eye. I verified without push and pop it does 
-        fill("#5c3427f2");
-        ellipse(275,275,30,30) //the brown eyeball
-        fill("#000000ff")
-        ellipse(275,275,15,15)  // the iris
-        fill("#fff")
-        ellipse(280,268,8,8) // the reflection
-        pop();
-        
-        //The right eye
-        ellipse(365, 275, 55, 30);
-        push();// Keeping the same consistency as the first eye
-        fill("#5c3427f2");
-        ellipse(365,275,30,30) //the brown eyeball
-        fill("#000000ff")
-        ellipse(365,275,15,15)  //the iris
-        fill("#fff")
-        ellipse(370,268,8,8) // the reflection
-        
-        pop();
-        
+    pop();
     }
     
     //drawing the nose
-    function Drawnose(){
-push();
-noFill();
-stroke("#2b2418ff");
-strokeWeight(2);
-arc(320,320,25,30,170,185)
-pop();
+function Drawnose(){
+    push();
+    noFill();
+    stroke("#2b2418ff");
+    strokeWeight(2);
+    arc(320,320,25,30,170,185)
+    pop();
 }
 
 
@@ -168,11 +157,10 @@ function Drawmouth(){
     arc(330,365, 40, 20, 154, 270.1);
     
     //Lower lip
-    ellipse(322,363, 50, 15,);
-    
-    pop();
-    
+    ellipse(322,363, 50, 15,); 
+    pop();    
 }
+
 //Drawing the Ears
 function Drawears(){
     push();
@@ -199,9 +187,9 @@ function Drawsweater(){
 
 
     pop();
-
-
 }
+
+
 }
 //**" */
 
