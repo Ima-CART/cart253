43bfe3261d153dce512828767c25ac0b590ce40e
Ima-CART <Sandra_Williams93@hotmail.com>
2025-10-02 22:49:59 -0400

    Spacing the functions
    Spacing all the arguments

--- Jams/Art-Jam/js/script.js
+++ Jams/Art-Jam/js/script.js
@@ -47,9 +47,9 @@ function draw() {
 //Conditional used in order to change the background depending on the mouse movement on the x-axis 
     function Drawbackground(){
       if (mouseX < 200){
-    background(166,249,255);
+    background(166, 249, 255);
   } else if (mouseX >= 200 && mouseX < 400){
-    background(176,149,232);
+    background(176, 149, 232);
   } else if (mouseX >= 400){
     background(255, 235, 235);
   } 
@@ -61,7 +61,7 @@ function Drawhair(){
     push();
     noStroke();
     fill("#957011ff");
-    ellipse(320,285,270,350); // This is the hair in the back.
+    ellipse(320, 285, 270, 350); // This is the hair in the back.
     pop();
 }
 
@@ -80,7 +80,7 @@ function Drawface(){
     push();
     noStroke();
     fill("#8d6241ff");
-    ellipse(320,280,200,270);
+    ellipse(320, 280, 200, 270);
     pop();
 }
     
@@ -88,8 +88,8 @@ function Drawface(){
 function Drawtshirt() {
     push();
     noStroke();
-    fill(mouseX,mouseY,10); //The inner tshirt will be changing colors
-    rect(180, 450, 280, 250,80);
+    fill(mouseX, mouseY, 10); //The inner tshirt will be changing colors
+    rect(180, 450, 280, 250, 80);
     fill("#f1f5ffff");
     ellipse(320, 460, 85, 65); //The neckline
     pop();
@@ -100,7 +100,7 @@ function Drawneck(){
     push();
     noStroke();
     fill("#8d6241ff");
-    rect(280, 350, 80, 130,40);
+    rect(280, 350, 80, 130, 40);
     pop();
        
 }
@@ -125,22 +125,22 @@ function Draweyes() {
     ellipse(275, 275, 55, 30); //The white part of the eye 
     push(); //I don't want the colors to fall on the second eye. I verified without push and pop it does 
     fill("#5c3427f2");
-    ellipse(275,275,30,30); //The brown eyeball
+    ellipse(275, 275, 30, 30); //The brown eyeball
     fill("#000000ff");
-    ellipse(275,275,15,15); //The iris
+    ellipse(275, 275, 15, 15); //The iris
     fill("#fff");
-    ellipse(280,268,8,8); //The reflection
+    ellipse(280, 268, 8, 8); //The reflection
     pop();
         
 //The right eye
     ellipse(365, 275, 55, 30); //The white part of the eye
     push(); //Keeping the same consistency as the first eye
     fill("#5c3427f2");
-    ellipse(365,275,30,30); //The brown eyeball
+    ellipse(365, 275, 30, 30); //The brown eyeball
     fill("#000000ff");
-    ellipse(365,275,15,15); //the iris
+    ellipse(365, 275, 15, 15); //the iris
     fill("#fff");
-    ellipse(370,268,8,8); //The reflection
+    ellipse(370, 268, 8, 8); //The reflection
     
     
     pop();
@@ -152,7 +152,7 @@ function Drawnose(){
     noFill();
     stroke("#2b2418ff");
     strokeWeight(2);
-    arc(320,320,20,20,0,180);
+    arc(320, 320, 20, 20, 0, 180);
     pop();
 }
 
@@ -165,11 +165,11 @@ function Drawmouth(){
     push();
     fill("#6e3313ff");
     noStroke();
-    arc(315,365, 40, 20, 180, 0);
-    arc(330,365, 40, 20, 180, 0);
+    arc(315, 365, 40, 20, 180, 0);
+    arc(330, 365, 40, 20, 180, 0);
     
 //Lower lip
-    ellipse(322,363, 50, 15); 
+    ellipse(322, 363, 50, 15); 
     pop();    
 }
 
@@ -177,8 +177,8 @@ function Drawmouth(){
 function Drawears(){
     push();
     fill("#8d6241ff");
-    ellipse(420,285,40,55); //The right ear
-    ellipse(220,285,40,55); //The left ear
+    ellipse(420, 285, 40, 55); //The right ear
+    ellipse(220, 285, 40, 55); //The left ear
     pop();
 }
 
@@ -207,20 +207,20 @@ function Drawbangs() {
     push();
     translate(365, 190);
     rotate(-55);
-    ellipse(0,0,100,170);
+    ellipse(0, 0, 100, 170);
     pop();
     push();
 
 //Left Bangs
      push();
-     translate(275,190);
+     translate(275, 190);
      rotate(55);
      ellipse(0, 0, 100, 175);
      pop();
 
  //Right strand
      beginShape();
-     translate(350,230);
+     translate(350, 230);
      vertex();
      bezierVertex(80, 0, 80, 75, 30, 120);
      bezierVertex(50, 80, 60, 25, 30, 20);
@@ -229,7 +229,7 @@ function Drawbangs() {
 
 //Left strand
      beginShape();
-     translate(200,225);
+     translate(200, 225);
      vertex();
      bezierVertex(10, 0, 0, 100, 60, 120);
      bezierVertex(10, 70, 40, 40, 60, 20);
