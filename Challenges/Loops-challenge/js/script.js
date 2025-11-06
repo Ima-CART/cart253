a568498df47ba5dca8000fb02b0277cad3982ede
Ima-CART <Sandra_Williams93@hotmail.com>
2025-11-06 15:55:16 -0500

    Add more lines
    Add vertical and horizontal lines
    with function
    step 1, 2, 3

--- Challenges/Loops-challenge/js/script.js
+++ Challenges/Loops-challenge/js/script.js
@@ -1,24 +1,76 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
 "use strict";
 
+let x = 0;
+let y = 0;
+let color = 0;
+let x2 = 500;
+
+
+
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(500, 500);
+    background("pink");
 
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws lines across the canvas with increasing thickness and
+ * gradually lightening colour
+ */
 function draw() {
+    color = 0;
+    while (x <= 500) {
+        stroke(color);
+        line(x, y, x, 500);
+        x += 50;
+        y = 0;
+        color += 25;
 
+    }
+    color = 0;
+    while (y <= 500) {
+        stroke(color);
+        line(x, y, x2, y);
+        x = 0;
+        y += 50;
+        color += 25;
+
+    }
+
+    /*stroke(0);
+    line(0, 0, 0, height);
+
+    stroke(25);
+    line(50, 0, 50, height);
+
+    stroke(50);
+    line(100, 0, 100, height);
+
+    stroke(75);
+    line(150, 0, 150, height);
+
+    stroke(100);
+    line(200, 0, 200, height);
+
+    stroke(125);
+    line(250, 0, 250, height);
+
+    stroke(150);
+    line(300, 0, 300, height);
+
+    stroke(175);
+    line(350, 0, 350, height);
+
+    stroke(200);
+    line(400, 0, 400, height);
+
+    stroke(225);
+    line(450, 0, 450, height);
+
+    stroke(250);
+    line(500, 0, 500, height);*/
 }
